ec7e54969422300271e5d2ebdf33ea26e5da84bb
jlkang <allen@grid-safe.com>
2017-03-26 22:55:18 +0800

    implement simple instant message function

--- server/web_socket.py
+++ server/web_socket.py
@@ -1,18 +1,28 @@
 # -*- coding: utf8 -*-
-import socket
+import socket;socket.socket.send()
 
 import gevent
-# from gevent import monkey; monkey.patch_all()
+from gevent import monkey; monkey.patch_all()
 
 
-class BaseWebSocket(object):
+class Client(object):
 
-    def __init__(self, client_sock, buf=1024):
+    def __init__(self, client_sock, session, username, buf=1024):
         self.sock = client_sock
         self.buf = buf
+        self.username = username
+        self.session = session
 
     def recv(self):
-        return self.sock.recv(self.buf)
+        while True:
+            data = self.sock.recv(self.buf)
+            if data.strip().decode() == 'quit':
+                self.sock.close()
+                self.session.delete(self.sock)
+                self.session.send('user{} has logout.'.format(self.username).encode())
+                continue
+            self.session.send('user{}:'.format(self.username).encode())
+            self.session.send(data)
 
     def send(self, data):
         self.sock.send(data)
@@ -31,6 +41,15 @@ class Session(object):
             raise TypeError('ws is not a socket instance.')
         self.web_sockets.append(ws)
 
+    def delete(self, sock):
+        if not isinstance(sock, socket.socket):
+            raise TypeError('ws is not a socket instance.')
+        self.web_sockets.remove(sock)
+
+    def send(self, data):
+        for s in self.web_sockets:
+            s.send(data)
+
 
 class Server(object):
 
@@ -44,15 +63,20 @@ class Server(object):
         print('start server...')
         self.server_sock.bind((self.host, self.port))
         self.server_sock.listen(self.listen)
+        session = Session()
+        user_id = 1
         try:
             while True:
                 print('waiting for connect...')
                 client_sock, addr = self.server_sock.accept()
                 print('connect from %s:%s' % addr)
-                client_sock.close()
-        except KeyboardInterrupt:
+                session.send('user{} has login.'.format(user_id).encode())
+                ws = Client(client_sock, session, user_id)
+                session.add(client_sock)
+                gevent.spawn(ws.recv)
+                user_id += 1
+        finally:
             self.server_sock.close()
-            raise KeyboardInterrupt('stop')
 
 if __name__ == '__main__':
     host, port = '0.0.0.0', 8090
